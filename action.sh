ee95e29fd8f4cd9ab37e7ef9533ab4461964233c
davizintech-ops <davizintech@gmail.com>
2026-09-17 18:01:33 -0300

    Initial integrityX V0.5.0

---
+++ action.sh
@@ -0,0 +1,17 @@
+MODPATH="${0%/*}"
+
+# ensure not running in busybox ash standalone shell
+if [ -n "$ASH_STANDALONE" ]; then
+    set +o standalone
+    unset ASH_STANDALONE
+fi
+
+sh $MODPATH/autopif4.sh -m || exit 1
+
+echo -e "\nDone!"
+
+# warn since KernelSU/APatch's implementation automatically closes if successful
+if [ "$KSU" = "true" -o "$APATCH" = "true" ] && [ "$KSU_NEXT" != "true" ] && [ "$KSU_SUKISU" != "true" ] && [ "$WKSU" != "true" ] && [ "$MMRL" != "true" ]; then
+    echo -e "\nClosing dialog in 20 seconds ..."
+    sleep 20
+fi
